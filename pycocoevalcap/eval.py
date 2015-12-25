d6c13363f91a03aacfad683b11f285e204d38652
rama <vrama91@vt.edu>
2015-12-25 16:54:31 -0500

    done with changes that enable evaluation with coco independent input data

--- pycocoevalcap/eval.py
+++ pycocoevalcap/eval.py
@@ -1,4 +1,9 @@
-__author__ = 'tylin'
+__author__ = 'vrama91'
+
+import os
+import json
+
+from collections import defaultdict
 from tokenizer.ptbtokenizer import PTBTokenizer
 from bleu.bleu import Bleu
 from meteor.meteor import Meteor
@@ -6,23 +11,43 @@ from rouge.rouge import Rouge
 from cider.cider import Cider
 
 class COCOEvalCap:
-    def __init__(self, coco, cocoRes):
-        self.evalImgs = []
+    def __init__(self, pathToData, refName, candName):
+        """
+        Reference file: list of dict('image_id': image_id, 'caption': caption).
+        Candidate file: list of dict('image_id': image_id, 'caption': caption).
+        :params: refName : name of the file containing references
+        :params: candName: name of the file containing cnadidates
+        """
         self.eval = {}
-        self.imgToEval = {}
-        self.coco = coco
-        self.cocoRes = cocoRes
-        self.params = {'image_id': coco.getImgIds()}
+        self._refName = refName
+        self._candName = candName
+        self._pathToData = pathToData
 
     def evaluate(self):
-        imgIds = self.params['image_id']
-        # imgIds = self.coco.getImgIds()
-        gts = {}
-        res = {}
-        for imgId in imgIds:
-            gts[imgId] = self.coco.imgToAnns[imgId]
-            res[imgId] = self.cocoRes.imgToAnns[imgId]
+        """
+        Load the sentences from json files
+        """
+        def readJson(refName, candName):
 
+            path_to_ref_file = os.path.join(self._pathToData, refName)
+            path_to_cand_file = os.path.join(self._pathToData, candName)
+
+            ref_list = json.loads(open(path_to_ref_file, 'r').read())
+            cand_list = json.loads(open(path_to_cand_file, 'r').read())
+
+            gts = defaultdict(list)
+            res = defaultdict(list)
+
+            for l in ref_list:
+                gts[l['image_id']].append({"caption": l['caption']})
+
+            for l in cand_list:
+                res[l['image_id']].append({"caption": l['caption']})
+
+            return gts, res
+
+        print 'Loading Data...'
+        gts, res = readJson(self._refName, self._candName)
         # =================================================
         # Set up scorers
         # =================================================
@@ -45,30 +70,15 @@ class COCOEvalCap:
         # =================================================
         # Compute scores
         # =================================================
-        eval = {}
         for scorer, method in scorers:
             print 'computing %s score...'%(scorer.method())
             score, scores = scorer.compute_score(gts, res)
             if type(method) == list:
                 for sc, scs, m in zip(score, scores, method):
                     self.setEval(sc, m)
-                    self.setImgToEvalImgs(scs, imgIds, m)
-                    print "%s: %0.3f"%(m, sc)
             else:
                 self.setEval(score, method)
-                self.setImgToEvalImgs(scores, imgIds, method)
-                print "%s: %0.3f"%(method, score)
-        self.setEvalImgs()
 
     def setEval(self, score, method):
         self.eval[method] = score
 
-    def setImgToEvalImgs(self, scores, imgIds, method):
-        for imgId, score in zip(imgIds, scores):
-            if not imgId in self.imgToEval:
-                self.imgToEval[imgId] = {}
-                self.imgToEval[imgId]["image_id"] = imgId
-            self.imgToEval[imgId][method] = score
-
-    def setEvalImgs(self):
-        self.evalImgs = [eval for imgId, eval in self.imgToEval.items()]
